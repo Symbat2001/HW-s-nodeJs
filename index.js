6ad519368109c83b7b8dc991ecfb841e2d2de98c
Symbat2001 <tazhben01@gmail.com>
2025-07-02 14:05:09 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db')
+const app = express();
+app.use(cors())
 app.use(express.json())
 const PORT = 3000
 
-const destinations = [
-    { id: 1, name: 'Париж', country: 'Франция', description: 'Жарықтар қаласы', price: 1200 },
-    { id: 2, name: 'Токио', country: 'Жапония', description: 'Күншығыс елі', price: 1500 }
-]
+app.get('/books', async (req, res) => {
+  try {
+    const result = await pool.query('select * from books');
+    res.json(result.rows);
+  } catch (e) {
+      res.status(500).json({error: 'Сервер қатесі'})
+  }
+});
 
-app.get('/destinations', (req, res) => {
-    res.json(destinations)
-})
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
 
-app.get('/destinations/:id', (req, res) => {
-    const id = req.params.id
-    const destination = destinations.find(d => d.id == parseInt(id))
-    if (!destination) {
-        res.status(404).json({ message: 'Бул бағыт табылмады!' })
+  try {
+    const result = await pool.query('SELECT * FROM books WHERE id = $1', [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).send('Кітап табылмады');
     }
-    res.json(destination)
-})
+    res.json(result.rows[0]);
+  } catch (e) {
+    res.status(404).json({error: 'Сервер қатесі'})
+  }
+});
 
-app.post('/destinations',(req, res) => {
-    const { name, country, description, price } = req.body
-
-    if (!name || !country || !description || !price) {
-        return res.status(400).json({ message: 'Барлық ақпарат толтырылмаған!' })
-    }
-
-    const newDestinations = {
-        id: destinations.length + 1,
-        name: name,
-        country: country,
-        description: description,
-        price: price
-    }
-
-    destinations.push(newDestinations)
-    res.json(newDestinations)
-})
-
-app.put('/destinations/:id', (req, res) => {
-    const id = req.params.id
-    const destination = destinations.find(d => d.id == parseInt(id))
-
-    if (!destination) {
-        res.status(404).json({ message: 'Бул бағыт табылмады!' })
-        
-    }
-    const { name, country, description, price } = req.body
-    destination.name = name
-    destination.country = country
-    destination.description = description
-    destination.price = price
-
-    res.json(destination)
-
-})
-
-app.delete('/destinations/:id', (req, res) => {
-    const id = req.params.id
-
-    const index = destinations.findIndex(d => d.id == parseInt(id))
-    if (index === -1) {
-        return res.status(404).json({ message: 'Бул бағыт табылмады!'})
-    }
-
-    destinations.splice(index,1)
-
-    return res.json({message: 'Бағыт өшірілді!'})
-})
-
-
-app.listen(PORT, () => {
+app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
 })
